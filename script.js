5559bb14926982abc5d5bed67e1f897fe21d6ec3
seherfatima <156211654+SeherFatima29@users.noreply.github.com>
2024-02-21 23:43:04 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,33 @@
+var sign=(prompt("click one sign"));
+var num1=parseInt(prompt("Enter first Number"));
+
+var num2=parseInt(prompt("Enter second Number"));
+
+
+if (sign == "+"){
+    alert(num1 + num2)      
+}
+else if(sign == "-"){
+    alert(num1 - num2)
+}
+else if (sign == "*"){
+   alert(num1 * num2)
+}
+else if(sign == "/"){
+    alert(num1 / num2)
+}
+else if (sign == "%"){
+   alert(num1 % num2)
+}
+alert(num3)
+
+
+
+
+
+
+
+
+
+
+
